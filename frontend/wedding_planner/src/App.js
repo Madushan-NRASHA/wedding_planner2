5c6d537f8c5727f0a53794209dac794db16f8d7f
Madushan-NRASHA <109310773+Madushan-NRASHA@users.noreply.github.com>
2024-11-05 14:23:48 +0530

    add files

--- frontend/wedding_planner/src/App.js
+++ frontend/wedding_planner/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+   <BrowserRouter>
+   <div>
+    <div className="navbar">
+      <h1>Wedding planner</h1>
+      <ul className='nav'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">About-us</Link></li>
+        <li><Link to="/">Portfolio</Link></li>
+        <li><Link to="/">Contact-us</Link></li>
+      </ul>
     </div>
+    </div>
+   </BrowserRouter>
   );
 }
 
