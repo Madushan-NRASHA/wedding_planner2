322acadf497a743429b82e178d790bcc42db7040
kavindu Nimalsha <125443904+Kavifdo2001@users.noreply.github.com>
2024-11-06 10:32:30 +0530

    backend user crud done

--- frontend/wedding_planner/src/App.js
+++ frontend/wedding_planner/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>hello</h1>
       </header>
     </div>
   );
